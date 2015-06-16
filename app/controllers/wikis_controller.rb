f2124cd32c56f97e42e2a185e70fd446fddf4e37
GoDucks713 <kizer713@gmail.com>
2015-06-16 18:23:14 +0000

    CRUD functions for Wikis done

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -14,18 +14,43 @@ class WikisController < ApplicationController
   end
 
   def edit
-  
+    @user = current_user
+    @wiki = Wiki.find(params[:id])
   end
 
   def create
-    @user = User.find(params[:user_id])
-    @wiki = Wiki.new(wiki_params)
+    @user = current_user
+    @wiki = @user.wikis.create(wiki_params)
     if @wiki.save
       flash[:notice] = "Wiki was saved"
       redirect_to @wiki
     else
       flash[:error] = "There was an error saving the Wiki. Please try again"
       render :new
+    end
+  end
+  
+  def update
+    @user = current_user
+    @wiki = Wiki.find(params[:id])
+    if @wiki.update_attributes(wiki_params)
+      redirect_to @wiki
+    else
+      flash[:error] = "Error saving wiki. Please try again"
+      render :edit
+    end
+  end
+  
+  def destroy
+    @user = current_user
+    @wiki = Wiki.find(params[:id])
+    title = @wiki.title
+    if @wiki.destroy
+      flash[:notice] = "\"#{title}\" was deleted successfully"
+      redirect_to root_path
+    else
+      flash[:error] = "There was an error deleting the wiki"
+      render :show
     end
   end
   
